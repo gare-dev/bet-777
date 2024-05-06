877042f3d5731670c00de42294ca65f6f0753de7
Lucas Amorim <lucasamorimsoke1215@gmail.com>
2024-05-06 12:54:54 -0300

    register done

--- action.php
+++ action.php
@@ -1,22 +1,22 @@
 <?php
-// Conectar ao banco de dados
-$host = 'localhost'; // endereço do servidor MySQL
-$usuario = 'root'; // usuário do banco de dados
-$senha = '4dMinForTheD4t4B4s3'; // senha do banco de dados
-$banco_de_dados = 'loginRegisterDb'; // nome do banco de dados
+
+$host = 'localhost';
+$usuario = 'root'; 
+$senha = '4dMinForTheD4t4B4s3'; 
+$banco_de_dados = 'loginRegisterDb'; 
 $conexao = new mysqli($host, $usuario, $senha, $banco_de_dados);
 
-// Verificar a conexão
+
 if ($conexao->connect_error) {
     die("Erro de conexão: " . $conexao->connect_error);
 }
 
-// Processar os dados do formulário
+
 $login = $_POST['logintxt'];
 $senha = $_POST['senha'];
 $dica = $_POST['dica'];
 
-// Inserir os dados no banco de dados
+// Inserir os dados no banco de dado
 $sql = "INSERT INTO login (login, senha, tip) VALUES ('$login', '$senha', '$dica')";
 
 if ($conexao->query($sql) === TRUE) {
---
+++ db/loginRegisterDb.sql
@@ -0,0 +1,14 @@
+create database loginRegisterDb;
+
+use loginRegisterDb;
+
+create table login (
+
+	id 			auto increment primary key,
+	login 		varchar(50),
+	senha 	varchar(50),
+	tip			varchar(50),
+	
+	
+);
+
